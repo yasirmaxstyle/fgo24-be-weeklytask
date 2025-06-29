8e87be24229076529f6297261c3c6d11145b4dce
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-29 08:46:50 +0700

    del: remove user_session entity struct since token jwt is used

--- models/users.go
+++ models/users.go
@@ -33,13 +33,3 @@ type Contact struct {
 	IsFavorite    bool      `json:"is_favorite" db:"is_favorite"`
 	CreatedAt     time.Time `json:"created_at" db:"created_at"`
 }
-
-type UserSession struct {
-	SessionID    int       `json:"session_id" db:"session_id"`
-	UserID       int       `json:"user_id" db:"user_id"`
-	SessionToken string    `json:"session_token" db:"session_token"`
-	ExpiresAt    time.Time `json:"expires_at" db:"expires_at"`
-	CreatedAt    time.Time `json:"created_at" db:"created_at"`
-	DeviceInfo   string    `json:"device_info" db:"device_info"`
-	IPAddress    string    `json:"ip_address" db:"ip_address"`
-}
