7a0d92b6fa21d711554a1fac816a878f8d142c66
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-29 08:21:04 +0700

    feat: add feature for getting user data by user ID

--- repositories/users.go
+++ repositories/users.go
@@ -94,3 +94,33 @@ func (r *UserRepository) GetUserByPhone(phone string) (*models.User, error) {
 	return &user, nil
 }
 
+func (r *UserRepository) GetUserByID(userID int) (*models.User, error) {
+	conn, err := utils.ConnectDB()
+	if err != nil {
+		return &models.User{}, err
+	}
+	defer utils.CloseDB(conn)
+
+	query := `
+		SELECT user_id, email, phone, full_name, password_hash, pin_hash, balance,
+			registration_status, is_verified, created_at, updated_at, last_login,
+			is_active, email_verification_token, phone_verification_token,
+			email_verified_at, phone_verified_at
+		FROM users WHERE user_id = $1 AND is_active = true`
+
+	var user models.User
+	err = conn.QueryRow(context.Background(), query, userID).Scan(
+		&user.UserID, &user.Email, &user.Phone, &user.FullName,
+		&user.PasswordHash, &user.PinHash, &user.Balance,
+		&user.RegistrationStatus, &user.IsVerified, &user.CreatedAt,
+		&user.UpdatedAt, &user.LastLogin, &user.IsActive,
+		&user.EmailVerificationToken, &user.PhoneVerificationToken,
+		&user.EmailVerifiedAt, &user.PhoneVerifiedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
