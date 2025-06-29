e4aa7b7e6e25d45f5013bd6909f649e99f8931e7
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-29 07:49:26 +0700

    feat: load port, running program on port 8080

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"backend-ewallet/routers"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -15,4 +16,14 @@ func main() {
 
 	r := gin.Default()
 	routers.CombineRouters(r)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on port %s", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
