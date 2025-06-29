1e59380a796a8e8cd506a55522e84c80cff2cf9d
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-29 08:12:34 +0700

    feat: add middleware for protecting path that requires token validation

---
+++ middlewares/auth.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"backend-ewallet/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+		userID, err := utils.ValidateToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", userID)
+		c.Next()
+	}
+}
