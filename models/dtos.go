ee3cc6982ecc67fcccec88648feb7fae24a2ed2c
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-29 22:59:50 +0700

    feat: add form tag for handling input

--- models/dtos.go
+++ models/dtos.go
@@ -1,26 +1,26 @@
 package models
 
 type RegisterRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Phone    string `json:"phone" binding:"required"`
-	FullName string `json:"full_name" binding:"required"`
-	Password string `json:"password" binding:"required,min=6"`
-	Pin      string `json:"pin" binding:"required,len=6"`
+	Email    string `form:"email" json:"email" binding:"required,email"`
+	Phone    string `form:"phone" json:"phone" binding:"required"`
+	FullName string `form:"full_name" json:"full_name" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required,min=8"`
+	Pin      string `form:"pin" json:"pin" binding:"required,len=6"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Email    string `form:"email" json:"email" binding:"required,email"`
+	Password string `form:"password" json:"password" binding:"required"`
 }
 
 type TransferRequest struct {
-	ReceiverPhone string  `json:"receiver_phone" binding:"required"`
-	Amount        float64 `json:"amount" binding:"required,gt=0"`
-	Description   string  `json:"description"`
-	Pin           string  `json:"pin" binding:"required,len=6"`
+	ReceiverPhone string  `form:"receiver_phone" json:"receiver_phone" binding:"required"`
+	Amount        float64 `form:"amount" json:"amount" binding:"required,gt=0"`
+	Description   string  `form:"description" json:"description"`
+	Pin           string  `form:"pin" json:"pin" binding:"required,len=6"`
 }
 
 type TopUpRequest struct {
-	Amount          float64 `json:"amount" binding:"required,gt=0"`
-	PaymentMethodID int     `json:"payment_method_id" binding:"required"`
+	Amount          float64 `form:"amount" json:"amount" binding:"required,gt=0"`
+	PaymentMethodID int     `form:"payment_method_id" json:"payment_method_id" binding:"required"`
 }
