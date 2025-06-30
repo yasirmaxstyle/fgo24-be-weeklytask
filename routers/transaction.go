0bb9cf6c76cf43b37115425e4f43810191bd28ea
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-30 11:24:47 +0700

    feat: add middleware authentication

--- routers/transaction.go
+++ routers/transaction.go
@@ -2,13 +2,16 @@ package routers
 
 import (
 	"backend-ewallet/controllers"
+	"backend-ewallet/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func transactionRouter(r *gin.RouterGroup) {
 	transactionController := controllers.NewTransactionController()
+	r.Use(middlewares.AuthMiddleware())
 
 	r.POST("/transfer", transactionController.Transfer)
+	r.POST("/topup", transactionController.Topup)
 	r.GET("/history", transactionController.GetTransactionHistory)
 }
