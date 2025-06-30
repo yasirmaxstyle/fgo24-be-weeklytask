99dd024f55c0558eef47b2dea25c34673a4d60ea
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-30 11:25:24 +0700

    feat: edit jwt construction

--- utils/jwt.go
+++ utils/jwt.go
@@ -5,39 +5,19 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 )
 
-var jwtSecret = []byte(os.Getenv("APP_SECRET"))
-
-type Claims struct {
-	UserID int    `json:"user_id"`
-	Email  string `json:"email"`
-	jwt.RegisteredClaims
-}
-
 func GenerateToken(userID int) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	godotenv.Load()
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := jwt.MapClaims{
 		"user_id": userID,
-		"exp":     time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	tokenString, err := token.SignedString(jwtSecret)
-	return tokenString, err
-}
-
-func ValidateToken(tokenString string) (int, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return jwtSecret, nil
-	})
-
-	if err != nil {
-		return 0, err
+		"iat":     time.Now().Unix(),
+		"exp":     expirationTime.Unix(),
 	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		userID := int(claims["user_id"].(float64))
-		return userID, nil
-	}
-
-	return 0, jwt.ErrInvalidKey
+	secretKey := os.Getenv("APP_SECRET")
+	return token.SignedString([]byte(secretKey))
 }
